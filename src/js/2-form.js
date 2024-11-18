0990a25ff718438bee2a9895812bf79c784fd792
Dmytro Chaika <djseagull90@gmail.com>
2024-11-18 23:08:19 +0100

    form end

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,13 +5,18 @@ let formData = {
     email: "",
     message: "",
 }
-    const localKay = "feedback-form-state"
+const localKay = "feedback-form-state"
 
 const form = document.querySelector(".feedback-form")
+console.dir(form)
+const input = document.querySelector('input[name="email"]');
+const texArea = document.querySelector('textarea[name="message"]');
 
-
+console.dir(input)
+console.dir(texArea)
 form.addEventListener("submit", toSubmit);
 form.addEventListener("input", onInput);
+isText()
 
 function onInput(evt) {
   
@@ -49,4 +54,18 @@ evt.preventDefault();
     this.reset();
   }
 }
+
+function isText() {
+  const inputMessage = localStorage.getItem(localKay)
+
+  if (inputMessage) {
+    const  inputMessageArr =JSON.parse(inputMessage)
+    input.value = inputMessageArr.email
+    texArea.value = inputMessageArr.message
+   
+}
+    
+}
+
+
 
