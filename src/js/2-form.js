002269b960ea89290c49a857c261d8915ee5cf0c
Dmytro Chaika <djseagull90@gmail.com>
2024-11-19 19:54:42 +0100

    finnish form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -40,20 +40,29 @@ evt.preventDefault();
     errorMsg.remove();
   }
 
-  if (!formData.email || !formData.message) {
+  if (!formData.email ) {
     const errorMessage = `<p class="errorMsg">Fill please all fields !!!</p>`;
     form.insertAdjacentHTML("beforeend", errorMessage);
-   
+       input.style.border = '2px solid red';
+
     
-  } else {
+  } else if(!formData.message) {
+    const errorMessage = `<p class="errorMsg">Fill please all fields !!!</p>`;
+    form.insertAdjacentHTML("beforeend", errorMessage);
+       texArea.style.border = '2px solid red';
+
+    
+  }else {
     
     console.log(formData);
        localStorage.removeItem('feedback-form-state');
        formData.email = "";
-       formData.message = "";
-    
+    formData.message = "";
+     input.style.border = '2px solid black';
+    texArea.style.border = '2px solid black';
     this.reset();
   }
+  
 }
 
 function isText() {
